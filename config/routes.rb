0c9201c52942fe1023051f7c2ab7edfcf93c0d4a
Danya D. Smith <danyadsmith@gmail.com>
2016-02-29 20:28:24 -0500

    Configure the API to Handle Versioning Through the 'Accept' HTTP Header
    Next, we modify the routes.rb file and include constraint for version 1
    of our API. Using the ApiConstraints class, we set the version of our
    api to 1 and set this as the default version.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 MarketPlaceApi::Application.routes.draw do
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
-    scope module: :v1 do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       
     end
   end
